94a114985c773eef502468d81517a6108379731c
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-14 11:06:38 +0700

    If ElSE

---
+++ if-else.js
@@ -0,0 +1,12 @@
+function jalanSatu() {
+  var nilai = 1;
+  if(nilai === 3){
+    console.log("Benar")
+  }else if(nilai === 2) {
+    console.log("Dua")
+  }else {
+    console.log("Salah")
+  }
+}
+
+jalanSatu()
