bb9c0e06b4165547ef3a79f4599f2f2be73e1f4d
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-15 09:22:07 +0700

    Array Dalam Objects

--- objects.js
+++ objects.js
@@ -3,10 +3,12 @@ function panggilObjects() {
     type: 'Sedan',
     harga: 2000000000,
     warna: 'Merah'
+    tahun: [2001,2002,2003,2004]
   }
 
   console.log(mobil)
-  console.log(mobil.warna)
+  console.log(mobil.harga)
+  console.log(mobil.tahun[3])
 }
 
 panggilObjects()
