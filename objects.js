f8825a59f36af980e73b896bd6ed421faac5dcee
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-15 09:08:10 +0700

    Objects

---
+++ objects.js
@@ -0,0 +1,12 @@
+function panggilObjects() {
+  var mobil = {
+    type: 'Sedan',
+    harga: 2000000000,
+    warna: 'Merah'
+  }
+
+  console.log(mobil)
+  console.log(mobil.warna)
+}
+
+panggilObjects()
