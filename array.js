2a13492e030c57fc76e282b2123c600df6b9a481
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-15 08:20:49 +0700

    Array

---
+++ array.js
@@ -0,0 +1,11 @@
+function panggilArray() {
+  var data = ["1,2,3,4,5,9"]
+  var nama = ["abi","abi1","abi2"]
+
+  console.log(data.length)
+  console.log(nama.length)
+
+  console.log(nama[1])
+}
+
+panggilArray()
