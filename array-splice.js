65ee41be1162b96b2dd57c29bbabddf67cc0914f
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-17 07:55:55 +0700

    Memanipulasi Array Dengan Splice Bagian 2

--- array-splice.js
+++ array-splice.js
@@ -1,7 +1,8 @@
 function panggilSplice() {
   var kota = ['Lampung','Jakarta','Malang','Solo','Medan'];
   console.log(kota)
-  kota.splice(0,3,'Jambi')
+  // kota.splice(0,3,'Jambi')
+  kota.splice(1,2)
   return kota
 }
 
