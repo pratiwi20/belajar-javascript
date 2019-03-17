14e83935a224d6441a7d5f0402888b121e805c68
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-17 09:13:11 +0700

    Perintah Reverse

---
+++ array-reverse.js
@@ -0,0 +1,10 @@
+function panggilReverse() {
+  var kota = ["Jakarta","Medan","Malang","Lampung"];
+  console.log(kota)
+  return kota.reverse()
+
+  //var result = kota.reverse();
+  //return result
+}
+
+console.log(panggilReverse())
