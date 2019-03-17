a0d3225fe22563e3e540bbf274851084c9abce0e
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-17 10:00:24 +0700

    Perulangan Dengan ForEach

---
+++ forEach.js
@@ -0,0 +1,10 @@
+function panggilforEach () {
+  var data = ['a','b','c'];
+  data.forEach(function(ai, au, ae) {
+    console.log(ai);
+    console.log(au);
+    console.log(ae);
+  })
+}
+
+panggilforEach()
