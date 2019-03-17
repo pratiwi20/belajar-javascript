d35c7e2432e6aad815b3ae8be265b68a3020f972
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-17 08:41:24 +0700

    Mengenal Perintah CONCAT

---
+++ array-concat.js
@@ -0,0 +1,9 @@
+function panggilConcat() {
+  var kota = ["Jakarta","Medan","Jambi","Bali"];
+  var kabupaten = ["Lampung tengah", "Kota Bumi", "Metro"];
+
+  var hasil = kota.concat(kabupaten)
+  return hasil
+}
+
+console.log(panggilConcat())
