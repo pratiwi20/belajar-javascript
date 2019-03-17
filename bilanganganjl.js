60d01c8e0121e3e5aea38e45b10d86ced868a0ef
pratiwi20 <wahyudwipratiwi8@gmail.com>
2019-03-17 09:39:59 +0700

    Bilangan Ganjil

---
+++ bilanganganjl.js
@@ -0,0 +1,11 @@
+function panggilBilanganGanjil() {
+  var bilanganGanjil = []
+  for (var i=1; i<30; i++){
+  if(i%2 !== 0) {
+    bilanganGanjil.push(i)
+  }
+}
+return bilanganGanjil
+}
+
+console.log(panggilBilanganGanjil())
